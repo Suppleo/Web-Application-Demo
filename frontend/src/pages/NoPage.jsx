d2f2f153d50debf63a6ea8005cdd078866b72e80
Nguyễn Ngọc Thạch <143619931+Suppleo@users.noreply.github.com>
2025-03-14 10:43:18 +0000

    add shadcn ui

--- frontend/src/pages/NoPage.jsx
+++ frontend/src/pages/NoPage.jsx
@@ -1,16 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 
 const NoPage = () => {
   return (
-    <div className="no-page-container">
+    <div className='no-page-container'>
       <h1>404</h1>
       <h2>Page Not Found</h2>
       <p>The page you are looking for doesn't exist or has been moved.</p>
-      <Link to="/" className="home-link">
+      <Link to='/' className='home-link'>
         Return to Homepage
       </Link>
     </div>
-  );
-};
+  )
+}
 
-export default NoPage;
+export default NoPage
